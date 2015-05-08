c1177fd59ea292f8804497d70dd4c2be23053383
Bobby Wang <wangbobby@gmail.com>
2015-05-08 18:01:00 -0400

    Add highlight-symbol.el

--- bob-pack/init.el
+++ bob-pack/init.el
@@ -6,6 +6,16 @@
 ;; Load bindings config
 (live-load-config-file "bindings.el")
 
+;; company mode
 (live-add-pack-lib "company-mode")
 (require 'company)
 (global-company-mode t)
+
+;; tramp
+(setq tramp-default-method "ssh")
+
+;; highlight symbol at cursor throughout buffer
+(live-add-pack-lib "highlight-symbol.el")
+(require 'highlight-symbol)
+(add-hook 'ruby-mode-hook 'highlight-symbol-mode)
+(add-hook 'clojure-mode-hook 'highlight-symbol-mode)
