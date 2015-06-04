88eb471618f31ab3c1f48e888ce924dfe091fbbb
bob <bob@bobs-MacBook-Pro.local>
2015-06-04 19:09:57 -0400

    Add haml and scss modes

--- bob-pack/init.el
+++ bob-pack/init.el
@@ -9,3 +9,17 @@
 (live-add-pack-lib "company-mode")
 (require 'company)
 (global-company-mode t)
+
+(live-add-pack-lib "scss-mode")
+(require 'scss-mode)
+(setq css-indent-offset 2)
+
+(live-add-pack-lib "haml-mode")
+(require 'haml-mode)
+
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+
+
+(setq exec-path (append exec-path '("~/bin")))
+
+(setq clojure-defun-style-default-indent t)
